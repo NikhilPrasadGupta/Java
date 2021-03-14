feeb480176fc9e99c7a00ec97cf98f1fee5acf58
unknown <nikhilgupta.jsp@gmail.com>
2021-03-14 21:13:22 +0530

    Program

---
+++ MethodDemo.java
@@ -0,0 +1,22 @@
+/*Name: Nikhil Prasad Gupta
+* Description: Demo of Method 
+*Date: 13/03/2021
+*/
+
+class MethodDemo{
+	public static void show(){
+		System.out.println("Show method");
+	}
+	public static int add(int num1,int num2){
+	return num1+num2;
+	
+	}
+	
+	public static void main(String[] args){
+		show();
+		show();
+		show();
+		int result=add(10,20);
+		System.out.println("Addition: "+result);
+	}
+}
