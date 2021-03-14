db8ae969ec1876985fea690046fa1c49f054f307
unknown <nikhilgupta.jsp@gmail.com>
2021-03-14 20:56:09 +0530

    Basic program

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+*Name: Nikhil Prasad Gupta
+*Description: Compilation & Running test
+*Date: 13/3/2021
+*/
+class HelloWorld{
+public static void main(String[] args){
+System.out.println("Hello World");
+}
+}
